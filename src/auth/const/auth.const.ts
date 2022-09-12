f5fcde09914dc8b3236cdccedd4f601a8b109175
Surov Evgeniy <surovjeniya@gmail.com>
2022-09-12 14:04:21 +0300

    feat:added auth module ,middleware and decorator

---
+++ src/auth/const/auth.const.ts
@@ -0,0 +1,11 @@
+const EMAIL_ALREADY_USED = 'Этот email уже используется';
+const NAME_ALREADY_USED = 'Этот логин уже используется.';
+const INVALID_PASSWORD = 'Неверный пароль';
+const INVALID_EMAIL = 'Неверный email';
+
+export {
+  EMAIL_ALREADY_USED,
+  NAME_ALREADY_USED,
+  INVALID_PASSWORD,
+  INVALID_EMAIL,
+};
